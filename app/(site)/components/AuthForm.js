5591d7d6d60b504b70761ca51457d415b7069bf2
saviodias1803 <savio.araujo@ufu.br>
2023-10-02 02:00:22 -0300

    Feat: html tela login

--- app/(site)/components/AuthForm.js
+++ app/(site)/components/AuthForm.js
@@ -10,7 +10,7 @@ import { useRouter } from 'next/navigation';
 async function loginUser(credentials) {
 
     console.log("AAAA");
-    if(credentials.username == "Teste" && credentials.password == 123) {
+    if (credentials.username == "Teste" && credentials.password == 123) {
         return "TRASDAWDAW";
     }
 
@@ -18,12 +18,12 @@ async function loginUser(credentials) {
     // Realiza request com servidor
 
     // se não possui login
-        // manda para cadastro
+    // manda para cadastro
     // se errou senha ou usario
-        // avisar
+    // avisar
     // se login 
-        // libera acesso
-    
+    // libera acesso
+
     // return fetch('http://localhost:8080/login', {
     //   method: 'POST',
     //   headers: {
@@ -41,45 +41,71 @@ export const AuthForm = () => {
 
     const router = useRouter();
 
-    const [username,setUsername] = useState("");
-    const [password,setPassword] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    useEffect( () => {
-        if(session.status == 'authenticated'){
+    useEffect(() => {
+        if (session.status == 'authenticated') {
             router.push('/users');
         }
-    },[router,session.status]);
+    }, [router, session.status]);
 
     const handleSubmit = async e => {
-      e.preventDefault();
+        e.preventDefault();
 
-      signIn('credentials',{
+        signIn('credentials', {
             email: username,
             password: password,
             redirect: false,
         }).then((callback) => {
-            if(callback.error) {
+            if (callback.error) {
                 toast.error("Erro ao realizar login: " + callback.error);
             }
-            if(callback.ok && !callback.error){
+            if (callback.ok && !callback.error) {
                 toast.success("Usuario logado com sucesso!");
                 router.push('/users');
             }
-      });
+        });
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <label>
-                <p>Username</p>
-                <input type="text" onChange={(event) => {setUsername(event.target.value)}} required/>
-            </label>
-            <label>
-                <p>Password</p>
-                <input type="password" onChange={(event) => {setPassword(event.target.value)}} required/>
-            </label>
-            <div>
-                <button type="submit">Submit</button>
+            <div style={{ width: 1440, height: 1024, position: 'relative', background: 'linear-gradient(0deg, #F5F5F5 0%, #F5F5F5 100%)', backgroundImage: 'url(/image/Rectangle3.png)' }}>
+                <div style={{ width: 1313, height: 892, left: 62, top: 66, position: 'absolute', background: 'white', borderRadius: 15, border: '1px #CCCCCC solid' }} />
+                <div style={{ width: 650, height: 892, left: 62, top: 66, position: 'absolute' }}>
+                    <div style={{ width: 650, height: 892, left: 0, top: 0, position: 'absolute', background: '#FF6347', borderTopLeftRadius: 15, borderTopRightRadius: 15 }} />
+                    <img style={{ width: 650, height: 892, left: 0, top: 0, position: 'absolute', borderTopLeftRadius: 15, borderTopRightRadius: 15 }} src='/image/Rectangle3.png' />
+                </div>
+                <div style={{ width: 512, height: 780, left: 793, top: 119, position: 'absolute' }}>
+                    {/* senha */}
+                    <div style={{ width: 512, height: 72, left: 0, top: 329, position: 'absolute' }}>
+                        <input type="password" onChange={(event) => { setPassword(event.target.value) }} required
+                            style={{ width: 501, left: 11, top: 27, position: 'absolute', color: '#332E2E', fontSize: 30, fontFamily: 'Roboto', fontWeight: '700', wordWrap: 'break-word', border: 'none', outline: 'none', borderBottom: '2px #332E2E solid' }} 
+                            placeholder="Senha" />
+                    </div>
+                    {/* usuário */}
+                    <div style={{ width: 512, height: 72, left: 0, top: 217, position: 'absolute' }}>
+                        <input type="text" onChange={(event) => { setUsername(event.target.value) }} required
+                            style={{ width: 501, left: 11, top: 27, position: 'absolute', color: '#332E2E', fontSize: 30, fontFamily: 'Roboto', fontWeight: '700', wordWrap: 'break-word', border: 'none', outline: 'none', borderBottom: '2px #332E2E solid' }} 
+                            placeholder="Usuário" />
+                    </div>
+                    <div style={{ width: 258, height: 63, left: 127, top: 453, position: 'absolute' }}>
+                        <div style={{ width: 258, height: 63, left: 0, top: 0, position: 'absolute', background: '#0EB36D', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 8, border: '1px rgba(198.89, 112.87, 112.87, 0.13) solid' }} />
+                        <button type="submit" 
+                            style={{ width: 258, height: 63, left: 0, top: 0, position: 'absolute', textAlign: 'center', color: '#F5F5F5', fontSize: 30, fontFamily: 'Roboto', fontWeight: '700', wordWrap: 'break-word' }}>
+                                Entrar
+                        </button>
+                    </div>
+                    <div style={{ width: 258, height: 63, left: 127, top: 717, position: 'absolute' }}>
+                        <div style={{ width: 258, height: 63, left: 0, top: 0, position: 'absolute', background: '#FF6347', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: 8, border: '1px rgba(198.89, 112.87, 112.87, 0.13) solid' }} />
+                        <div style={{ width: 258, height: 63, left: 0, top: 0, position: 'absolute', textAlign: 'center', color: '#F5F5F5', fontSize: 30, fontFamily: 'Roboto', fontWeight: '700', wordWrap: 'break-word' }}>Cadastre-se</div>
+                    </div>
+                    <div style={{ width: 258, left: 127, top: 529, position: 'absolute', textAlign: 'center', color: '#332E2E', fontSize: 20, fontFamily: 'Roboto', fontWeight: '400', textDecoration: 'underline', wordWrap: 'break-word' }}>Esqueceu sua senha?</div>
+                    <div style={{ width: 282, left: 115, top: 681, position: 'absolute', textAlign: 'center', color: '#332E2E', fontSize: 20, fontFamily: 'Roboto', fontWeight: '400', wordWrap: 'break-word' }}>Não possui uma conta ainda?</div>
+                    <img style={{ width: 240, height: 189, left: 127, top: 0, position: 'absolute' }} src="/image/Logo Chef Virtual.png" />
+                </div>
+                <img style={{ width: 48, height: 82, left: 1273, top: 66, position: 'absolute' }} src="/image/Vector.png" />
+                <div style={{ width: 584, height: 376, left: 95, top: 322, position: 'absolute', textAlign: 'center', color: 'white', fontSize: 45, fontFamily: 'Roboto', fontWeight: '700', wordWrap: 'break-word' }}>Precisou de ajuda na cozinha? Deixa com o Chef!</div>
             </div>
         </form>
     )
--- app/(site)/page.js
+++ app/(site)/page.js
@@ -3,7 +3,6 @@ import AuthForm from "./components/AuthForm"
 export default function Home() {
   return(
     <div>
-      <h1>Please Log In</h1>
       <AuthForm/>
     </div>
   )
